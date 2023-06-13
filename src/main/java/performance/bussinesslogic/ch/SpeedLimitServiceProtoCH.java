77e125b0102ec8df917a34cc7b01219983be306f
viktor.reinok <viktor.reinok@ipfdigital.com>
2023-06-13 21:58:11 +0200

    up spring version and commit recent changes

--- src/main/java/performance/bussinesslogic/ch/SpeedLimitServiceProtoCH.java
+++ src/main/java/performance/bussinesslogic/ch/SpeedLimitServiceProtoCH.java
@@ -1,12 +1,13 @@
 package performance.bussinesslogic.ch;
 
 import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import performance.bussinesslogic.SpeedLimitServiceProto;
 import performance.contition.CountryCH;
 
-//@Scope(value = org.springframework.web.context.WebApplicationContext.SCOPE_REQUEST)
 @Service
+@Scope(value = org.springframework.web.context.WebApplicationContext.SCOPE_REQUEST)
 @Conditional(CountryCH.class)
 public class SpeedLimitServiceProtoCH implements SpeedLimitServiceProto {
 
