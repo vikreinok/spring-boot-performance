54a42401fb751656084b03685320be4da0708eff
Viktor Reinok <vikreinok@gmail.com>
2023-09-25 22:40:53 +0200

    fix tests

--- src/main/java/performance/bussinesslogic/ch/SpeedLimitServiceProtoCH.java
+++ src/main/java/performance/bussinesslogic/ch/SpeedLimitServiceProtoCH.java
@@ -1,13 +1,12 @@
 package performance.bussinesslogic.ch;
 
 import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import performance.bussinesslogic.SpeedLimitServiceProto;
 import performance.contition.CountryCH;
 
 @Service
-@Scope(value = org.springframework.web.context.WebApplicationContext.SCOPE_REQUEST)
+//@Scope(value = org.springframework.web.context.WebApplicationContext.SCOPE_REQUEST)
 @Conditional(CountryCH.class)
 public class SpeedLimitServiceProtoCH implements SpeedLimitServiceProto {
 
--- src/main/java/performance/rest/Scenario4WorkaroundController.java
+++ src/main/java/performance/rest/Scenario4WorkaroundController.java
@@ -1,12 +1,14 @@
 package performance.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import performance.bussinesslogic.SpeedLimitServiceProto;
 
 import java.util.List;
 
+@Profile("no_load")
 @RestController
 @RequestMapping("/scenario4workaround")
 public class Scenario4WorkaroundController {
